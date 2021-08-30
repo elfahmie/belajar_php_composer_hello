ce412949da785d278840f2288f4d18080a0eabbd
elfahmie <you@example.com>
2021-08-30 22:43:10 +0700

    contoh library composer sederhana

---
+++ src/customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace belajarprogramming\belajar;
+
+class customer
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "hello $name, my name is $this->name";
+    }
+}
